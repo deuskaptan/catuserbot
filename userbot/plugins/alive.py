3526175ecb5012475ac09b548ad43629912b3376
deuskaptan <65902227+deuskaptan@users.noreply.github.com>
2020-06-08 19:04:09 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,7 +13,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY BOT IS RUNNING SUCCESFULLY**\n\n"
+    await alive.edit("**yarrauserbot çalışıyor**\n\n"
                      f"`☞Telethon version: {version.__version__}\n`"
                      f"`☞Python: {python_version()}\n`"
                      "`☞Bot was modified by:` sandeep\n"
